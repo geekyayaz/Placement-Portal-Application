986955712947176c3f5d55a145726c78c009fc5e
geekyayaz <your-ayazraikha@gmail.com>
2026-03-02 20:53:39 +0530

    Milestone-0 PPA-MAD-1

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from backend.models import db
+def create_app():
+    app=Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.sqlite"
+    db.init_app(app)
+    app.app_context().push()
+    return app
+
+app=create_app()
+from backend.routes import *
+if __name__=="__main__":
+    app.run(debug=True)
